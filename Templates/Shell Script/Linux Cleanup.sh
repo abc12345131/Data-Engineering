d2b5ca8e4293d90c6113f7c4600f87070473dbac
abc12345131 <52730527+abc12345131@users.noreply.github.com>
2020-09-13 02:37:41 -0400

    add shell script template

---
+++ Templates/Shell Script/Linux Cleanup.sh
@@ -0,0 +1,12 @@
+
+sudo apt-get update && sudo apt-get upgrade
+
+sudo apt-get autoremove
+
+sudo rm -rf ~/.cache/thumbnails/*
+
+du -sh ~/.cache/thumbnails
+
+sudo du -sh /var/cache/apt
+
+sudo apt-get clean
